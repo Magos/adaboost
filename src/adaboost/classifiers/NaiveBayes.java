a1551cf857f542a5c8cbe3d7708cb27b5e075295
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2012-11-19 12:04:34 +0100

    Made a pre-processing method in NBC.

--- src/adaboost/classifiers/NaiveBayes.java
+++ src/adaboost/classifiers/NaiveBayes.java
@@ -19,6 +19,8 @@ public class NaiveBayes<T extends Enum<T>> implements Classifier<T> {
 	private Map<ValueClassPair,Double> aPosteriori;
 	private Set<Instance<Enum<?>>> trainingSet;
 	private static int binCount;
+	/** Mappers for translating real-valued attributes into ints. */
+	private Map<Integer,Mapper> mappers = new HashMap<Integer,Mapper>();
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -61,13 +63,7 @@ public class NaiveBayes<T extends Enum<T>> implements Classifier<T> {
 			
 		}
 		//Preprocess to identify attribute ranges and create discretization scheme.
-		int attributeCount = 0;
-		Iterator<Object> attributes = trainingSet.iterator().next().getAttributes();
-		while(attributes.hasNext()){
-			attributeCount++;
-			attributes.next();
-		}
-		
+		preprocess(trainingSet);
 		//For each per-class set, count instances of each discrete attribute value.
 		for (Entry<T, Set<Instance<Enum<?>>>> group : partition.entrySet()) {
 			T classification = group.getKey();
@@ -78,11 +74,38 @@ public class NaiveBayes<T extends Enum<T>> implements Classifier<T> {
 		}
 	}
 
+	/** Preprocess a training set to set up mappers that discretize any real-valued attributes. */
+	private void preprocess(Set<Instance<Enum<?>>> trainingSet) {
+		Iterator<Object> attributes = trainingSet.iterator().next().getAttributes();
+		int attributeCount = 0;
+		Map<Integer,Class<?>> activeClasses = new HashMap<Integer,Class<?>>();
+		while(attributes.hasNext()){
+			attributeCount++;
+			Object next = attributes.next();
+			Class<?> activeClass= next.getClass();
+			activeClasses.put(attributeCount,activeClass);
+			if(next instanceof Double){
+				mappers.put(attributeCount, new Mapper((double)next, (double)next));
+			}
+		}
+		for (Instance<Enum<?>> instance : trainingSet) {
+			int attribute = 0;
+			for (Iterator it = instance.getAttributes(); it.hasNext();) {
+				Object next = it.next();
+				attribute++;
+				if(next instanceof Double){
+					mappers.get(attribute).update((double)next);
+				}
+			}
+		}
+	}
+
 	@Override
 	public void configure(Properties props, String prefix) {
-		binCount = props.getIntProperty(prefix+"bins");
+		binCount = Math.abs(props.getIntProperty(prefix+"bins"));
 	}
 	
+	/** A collection class for holding the information needed to look up an attribute probability.*/
 	private class ValueClassPair{
 		int position = 0;
 		Object attribute;
@@ -101,6 +124,25 @@ public class NaiveBayes<T extends Enum<T>> implements Classifier<T> {
 						attribute.equals(other.attribute) &&
 						classification == other.classification;
 			}else return false;
+		}
+	}
+	
+	/** A support class for discretizing real-valued attributes. */
+	private class Mapper{
+		double max, min;
+		
+		Mapper(double max, double min){
+			this.max = max;
+			this.min = min;
+		}
+		
+		int map(double in){
+			return (int) ((in - min)*binCount/max);
+		}
+		
+		void update(double in){
+			if(in < min) min = in;
+			if(in > max) max = in;
 		}
 	}
 }
